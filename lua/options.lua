e04660e3cf3448b77538c3263f7782cd56222b49
OleksandrKirkov <kirkovsasha009@gmail.com>
2026-03-21 01:32:02 +0200

    feat: setting nvchad options

--- lua/options.lua
+++ lua/options.lua
@@ -1,1 +1,39 @@
 require "nvchad.options"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+vim.diagnostic.config {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = function()
+    vim.cmd "echo ''"
+  end,
+})
+
+vim.api.nvim_create_autocmd("TermClose", {
+  pattern = "*",
+  callback = function(args)
+    vim.defer_fn(function()
+      if vim.api.nvim_buf_is_valid(args.buf) and vim.bo[args.buf].buftype == "terminal" then
+        vim.api.nvim_buf_delete(args.buf, { force = true })
+      end
+    end, 5)
+  end,
+})
+
+local original_close = vim.api.nvim_win_close
+vim.api.nvim_win_close = function(winid, force)
+  pcall(original_close, winid, force)
+end
